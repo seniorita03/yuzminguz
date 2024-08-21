360e05edc09e87e8d3b2b66b505f3ff350a7d0ac
Asus <shuhratabduraxmonov08@gmail.com>
2024-08-21 11:54:30 +0500

    done

--- apps/models.py
+++ apps/models.py
@@ -1,2 +1,62 @@
-from django.db import models
+from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.models import AbstractUser
+from django.db.models import TextChoices, Model, DateTimeField, CharField, SlugField
+from django.utils.text import slugify
 
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, phone_number, password=None, **extra_fields):
+        if not phone_number:
+            raise ValueError('The Phone Number field must be set')
+        user = self.model(phone_number=phone_number, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, phone_number, password, **extra_fields):
+        user = self.create_user(phone_number, password, **extra_fields)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+        return user
+
+
+class BaseModel(Model):
+    created_at = DateTimeField(auto_now_add=True)
+    updated_et = DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class BaseSlugModel(Model):
+    name = CharField(max_length=255)
+    slug = SlugField(unique=True)
+
+    class Meta:
+        abstract = True
+
+    def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
+        self.slug = slugify(self.name)
+        while self.__class__.objects.filter(slug=self.slug).exists():
+            self.slug += '-1'
+        super().save(force_insert, force_update, using, update_fields)
+
+    def __str__(self):
+        return self.name
+
+
+class User(AbstractUser):
+    class Role(TextChoices):
+        ADMIN = "admin", 'Admin'
+        OPERATOR = "operator", 'Operator'
+        MANAGER = "manager", 'Manager'
+        DRIVER = "driver", 'Driver'
+        USER = "user", 'User'
+
+    username = None
+    USERNAME_FIELD = 'phone_number'
+    REQUIRED_FIELDS = []
+    objects = CustomUserManager()
+    role = CharField(max_length=50, choices=Role.choices, default=Role.USER)
+    phone_number = CharField(max_length=12, unique=True)
